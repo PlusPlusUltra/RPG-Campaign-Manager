2fce3c8a3fe8baad343101e25675551ab44b39cc
pivarim <pivari.1752071@studenti.uniroma1.it>
2019-05-08 17:19:37 +0200

    creazione pagine di index e home e inizializzazione del processo di login

--- RCM/config/environments/development.rb
+++ RCM/config/environments/development.rb
@@ -49,6 +49,7 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
@@ -58,4 +59,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
 end
