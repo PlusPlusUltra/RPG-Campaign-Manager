9ba44b4a7b43219423d293902ac352e165d30035
pivarim <pivarim@gmail.com>
2019-05-14 20:14:52 +0200

    configurazione mail per reimpostarla

--- RCM/db/schema.rb
+++ RCM/db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2019_05_14_134323) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
