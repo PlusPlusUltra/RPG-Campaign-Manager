fb149dd019dc6f8359299e46b5453f998b9d0162
pivarim <pivari.1752071@studenti.uniroma1.it>
2019-05-14 16:35:36 +0200

    risolto tutti i problemi con login

--- RCM/app/models/user.rb
+++ RCM/app/models/user.rb
@@ -9,12 +9,23 @@ class User < ActiveRecord::Base
     def login
     	@login || self.username || self.email
   	end
-  	
+
     validates :username, presence: :true, uniqueness: { case_sensitive: false }
 
     validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 
     has_many :authorizations
+
+    def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_hash).first
+      end
+    end
+
+
 
     # omniauth facebook provider
     def self.from_omniauth(auth, current_user)
@@ -28,14 +39,6 @@ class User < ActiveRecord::Base
     ).first_or_initialize
 
 
-    def self.find_for_database_authentication(warden_conditions)
-      conditions = warden_conditions.dup
-      if login = conditions.delete(:login)
-        where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-        where(conditions.to_h).first
-      end
-    end
 
     
 
