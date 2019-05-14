564920b189469a64e281ccc97721c21c0e880c64
pivarim <pivari.1752071@studenti.uniroma1.it>
2019-05-14 18:02:10 +0200

    tolto campo email dal login

--- RCM/app/models/user.rb
+++ RCM/app/models/user.rb
@@ -3,14 +3,15 @@ class User < ActiveRecord::Base
   	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   	devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable,
-        :omniauthable, :omniauth_providers => [:google_oauth2]
+        :omniauthable, :omniauth_providers => [:google_oauth2], authentication_keys: [:login]
     attr_writer :login
 
     def login
-    	@login || self.username || self.email
+    	@login  || self.email || self.username
   	end
 
-    validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  	validates :email, uniqueness: true
+    validates :username, uniqueness: { case_sensitive: false }
 
     validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 
@@ -19,9 +20,9 @@ class User < ActiveRecord::Base
     def self.find_for_database_authentication(warden_conditions)
       conditions = warden_conditions.dup
       if login = conditions.delete(:login)
-        where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+        where(conditions.to_h).where(["username = :value OR email = :value", { :value => login.downcase }]).first
       elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-        where(conditions.to_hash).first
+        where(conditions.to_h).first
       end
     end
 
