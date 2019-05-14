6bbf7f722ed124c2358f52b71f1be30a164332eb
pivarim <pivari.1752071@studenti.uniroma1.it>
2019-05-14 15:49:39 +0200

    aggiunto username all' utente cosi ci si può loggare con username o email

--- RCM/app/models/user.rb
+++ RCM/app/models/user.rb
@@ -4,6 +4,11 @@ class User < ActiveRecord::Base
   	devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable,
         :omniauthable, :omniauth_providers => [:google_oauth2]
+    attr_writer :login
+
+    validates :username, presence: :true, uniqueness: { case_sensitive: false }
+
+    validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 
     def self.from_omniauth(auth)
     	where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
@@ -16,6 +21,18 @@ class User < ActiveRecord::Base
     	end
     end
 
+    def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+      end
+    end
+
+    def login
+    	@login || self.username || self.email
+  	end
     
 
 end
