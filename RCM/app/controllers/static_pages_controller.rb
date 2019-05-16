79f0fa349a95d089c45e3205df27b9840b7361a3
pivarim <you@example.com>
2019-05-16 14:04:47 +0200

    risolto bug

--- RCM/app/controllers/static_pages_controller.rb
+++ RCM/app/controllers/static_pages_controller.rb
@@ -27,14 +27,15 @@ private
 
 	def check
 		if (params[:action] == "index" || params[:action] == "simple_character") && user_signed_in?
-			if current_user.email =="pivarim@gmail.com"
-				current_user.update_attribute :admin, true
-			end
+			
 			redirect_to static_pages_home_url
 		end
 		if params[:action] == "show_all_users" && !current_user.admin?
 			redirect_to static_pages_home_url
 		end
+		if user_signed_in? && !current_user.admin? && current_user.email =="pivarim@gmail.com"
+				current_user.update_attribute :admin, true
+			end
 	end
 
 end
